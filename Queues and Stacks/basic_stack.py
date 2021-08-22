ad44debe8ed63161a2355df1e319d3e20899ac6b
dvir200 <76834454+dvir200@users.noreply.github.com>
2021-08-22 10:16:43 +0300

    Create basic_stack.py

---
+++ Queues and Stacks/basic_stack.py
@@ -0,0 +1,19 @@
+
+""" Stack - Last in, First out """
+
+stack = []
+
+stack.append("data1")
+stack.append("data2")
+stack.append("data3")
+stack.append(5)
+
+print(stack)
+
+""" Delete data4 """
+stack.pop()
+print(stack)
+
+""" Delete data3 """
+stack.pop()
+print(stack)
