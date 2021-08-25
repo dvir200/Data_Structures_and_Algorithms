37e18992be53afb1fc58de2ca023f0b1b0e9ab9b
dvir200 <76834454+dvir200@users.noreply.github.com>
2021-08-25 11:56:06 +0300

    Create BasicClosedHash.py

---
+++ Hash Maps/BasicClosedHash.py
@@ -0,0 +1,14 @@
+string = "Zoe"
+print("Before ASCII code:")
+print(string)
+print()
+
+
+
+result = 0
+result = result + ord(string[0])
+result = result + ord(string[1])
+result = result + ord(string[2])
+
+print("After ASCII code:")
+print(result)
