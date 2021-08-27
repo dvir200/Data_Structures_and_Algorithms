3d1be8527dfc854fef3c603a06ed65254017f547
dvir200 <76834454+dvir200@users.noreply.github.com>
2021-08-27 11:13:33 +0300

    Update BasicClosedHash.py

--- Hash Maps/BasicClosedHash.py
+++ Hash Maps/BasicClosedHash.py
@@ -1,14 +1,69 @@
-string = "Zoe"
-print("Before ASCII code:")
-print(string)
-print()
+""" node builder function """
+class node:
+  def __init__(self, name, age, role):
+      self.name = name
+      self.age = age
+      self.role = role
+      self.next = None
 
 
+""" basis for the linked list """
+class linkedList:
+  def __init__(self):
+      self.head = None
+  
+  """ inserts a node in a given arr position """
+  def insertNode (self, name, age, role):
+    if self.head == None:
+      self.head = node(name, age, role)
+      return
+    else:
+      tmp = self.head
+      prev = None
+      while tmp:
+        prev = tmp
+        tmp = tmp.next
+      prev.next = node(name, age, role)
+      return
+  
+  """ display the linked list in a given arr position """
+  def display(self):
+    tmp = self.head
+    while tmp:
+      print(tmp.data)
+      tmp = tmp.next
+    return
 
-result = 0
-result = result + ord(string[0])
-result = result + ord(string[1])
-result = result + ord(string[2])
+""" function that will initilize all places in a array to be type of linked list object """
+def initilize(arr, length):
+  for x in range (len(arr)):
+    arr[x] = linkedList()
+  return arr
 
-print("After ASCII code:")
-print(result)
+
+""" exchange the name of the employee to a ASCII code """
+def exchangeToPos(name, arrLength):
+  result = 0
+  for x in range(len(name)):
+    result = result + ord(name[x])
+  result = result % arrLength
+  return result
+
+""" main function that will automate anything """
+def main(arr, name, age, role):
+  arr = initilize(arr, len(arr)-1)
+  pos = exchangeToPos(name, len(arr))
+  arr[pos].insertNode(name, age, role)
+  return
+
+
+if __name__ == '__main__':
+
+  dataArr = [None, None, None, None, None, None]
+  print(len(dataArr))
+  """ lengthArr = 6 """
+
+  dataArr = main(dataArr, "Zoe", 21, "Head of marketing team")
+  dataArr = main(dataArr, "Sebastian", 34, "Head of IT team")
+
+
